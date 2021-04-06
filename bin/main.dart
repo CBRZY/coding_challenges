3fbf9fdf7553c98c6d8ab9b88b66cecf98386824
Digital Capitán <chris@digitalcapitan.com>
2021-04-06 17:17:29 +1000

    Inital commit for coding challenges

---
+++ bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:coding_challenges/fibonacci/fibonacci.dart' as fibonacci;
+
+void main(List<String> arguments) {
+  print('Hello world: ${fibonacci.calculate()}!');
+}
---
+++ test/coding_challenges_test.dart
@@ -0,0 +1,8 @@
+import 'package:coding_challenges/test.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
