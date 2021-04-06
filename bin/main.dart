3341ea44e8da5685ab6d822cfb35e63dae706eb2
Digital Capitán <chris@digitalcapitan.com>
2021-04-06 22:20:51 +1000

    Added execution method to track time it takes

--- bin/main.dart
+++ bin/main.dart
@@ -1,11 +1,14 @@
 import 'package:coding_challenges/1_fibonacci/fibonacci.dart';
 
 void main(List<String> arguments) {
-  var fibN = 20;
+  var fibN = 42;
   executeChallenge(fibonacci, 'The ${fibN}th Fibonacci number is:', [fibN]);
+  executeChallenge(fibonacciRecursive, 'The ${fibN}th Fibonacci number (using recursion) is:', [fibN]);
 }
 
 void executeChallenge(Function func, String stringToPrint, List<dynamic> arguments) {
+  var timeBefore = DateTime.now().microsecondsSinceEpoch;
   print('$stringToPrint ${func(arguments[0])}');
-
+  var timeAfter = DateTime.now().microsecondsSinceEpoch;
+  print('Executed in ${timeAfter - timeBefore} microseconds (10^-6)');
 }
