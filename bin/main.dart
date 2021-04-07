2f1ea4f66dad6afade9a02939c564d205274b116
Digital Capitán <chris@digitalcapitan.com>
2021-04-07 21:12:00 +1000

    Initial commit for RobotCleaner challenge. Creation and printing of cleaning map implemented

--- bin/main.dart
+++ bin/main.dart
@@ -1,14 +1,28 @@
 import 'package:coding_challenges/1_fibonacci/fibonacci.dart';
+import 'package:coding_challenges/2_robot_cleaner/robot_cleaner.dart';
 
 void main(List<String> arguments) {
-  var fibN = 42;
+  // fibonacci();
+  robotCleaner();
+}
+
+void executeChallenge(Function func, String stringToPrint, List<dynamic> arguments) {
+  int timeBefore = DateTime.now().microsecondsSinceEpoch;
+  print('$stringToPrint ${func(arguments[0])}');
+  int timeAfter = DateTime.now().microsecondsSinceEpoch;
+  print('Executed in ${timeAfter - timeBefore} microseconds (10^-6)');
+}
+
+void fibonacci() {
+  int fibN = 42;
   executeChallenge(fibonacci, 'The ${fibN}th Fibonacci number is:', [fibN]);
   executeChallenge(fibonacciRecursive, 'The ${fibN}th Fibonacci number (using recursion) is:', [fibN]);
 }
 
-void executeChallenge(Function func, String stringToPrint, List<dynamic> arguments) {
-  var timeBefore = DateTime.now().microsecondsSinceEpoch;
-  print('$stringToPrint ${func(arguments[0])}');
-  var timeAfter = DateTime.now().microsecondsSinceEpoch;
-  print('Executed in ${timeAfter - timeBefore} microseconds (10^-6)');
+void robotCleaner() {
+  int x = 10, y = 10;
+  List<List<String>> newMap = List<List<String>>.generate(x, (_) => List.filled(y, '.'));
+  RobotCleaner rc = RobotCleaner(cleaningMap: newMap);
+  rc.start();
+  rc.printMap();
 }
---
+++ lib/2_robot_cleaner/robot_cleaner.dart
@@ -0,0 +1,33 @@
+class StartingPosition {
+    final int x;
+    final int y;
+
+    const StartingPosition({this.x = 0, this.y = 0});
+}
+
+class RobotCleaner {
+  
+  List<List<String>>? cleaningMap;
+  final StartingPosition startingPos;
+
+  RobotCleaner({this.cleaningMap, this.startingPos = const StartingPosition()});
+
+  void printMap() {
+    cleaningMap?.forEach((row) { 
+      String displayRow = row.fold('', (prev, curr) => '$prev $curr');
+      print(displayRow);
+    });
+  }
+
+  void start() {
+    cleaningMap?[startingPos.y][startingPos.x] = 'R';
+    cleaningMap != null ? traverseMap() : null;
+  }
+
+
+  void traverseMap() {
+    
+  }
+
+  
+}
