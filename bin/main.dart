03f274b77d55659744087bb09dc44beb1134fc88
DigitalCapitan <captain@cbrzy.com>
2021-04-08 21:20:54 +1000

    Basic Robot Cleaner working. No extras yet. Still need to clean up code

--- bin/main.dart
+++ bin/main.dart
@@ -6,6 +6,7 @@ void main(List<String> arguments) {
   robotCleaner();
 }
 
+// Call challenge methods with executeChallenge to see the time it takes to complete the solution
 void executeChallenge(Function func, String stringToPrint, List<dynamic> arguments) {
   int timeBefore = DateTime.now().microsecondsSinceEpoch;
   print('$stringToPrint ${func(arguments[0])}');
@@ -13,16 +14,19 @@ void executeChallenge(Function func, String stringToPrint, List<dynamic> argumen
   print('Executed in ${timeAfter - timeBefore} microseconds (10^-6)');
 }
 
+// Challenge 1: Fibonacci => Find the nth Fibonacci number
 void fibonacci() {
   int fibN = 42;
+
   executeChallenge(fibonacci, 'The ${fibN}th Fibonacci number is:', [fibN]);
   executeChallenge(fibonacciRecursive, 'The ${fibN}th Fibonacci number (using recursion) is:', [fibN]);
 }
 
+// Challenge 2: Robot Cleaner => Traverse a 2D array with a robot
 void robotCleaner() {
   int x = 10, y = 10;
   List<List<String>> newMap = List<List<String>>.generate(x, (_) => List.filled(y, '.'));
   RobotCleaner rc = RobotCleaner(cleaningMap: newMap);
-  rc.start();
-  rc.printMap();
+
+  executeChallenge(rc.start, 'The amount of steps the Robot took to clean the map is:', [true]);
 }
--- lib/2_robot_cleaner/robot_cleaner.dart
+++ lib/2_robot_cleaner/robot_cleaner.dart
@@ -1,33 +1,109 @@
-class StartingPosition {
+import 'dart:io';
+
+class Position {
     final int x;
     final int y;
 
-    const StartingPosition({this.x = 0, this.y = 0});
+    const Position({this.x = 0, this.y = 0});
+    Position.from(int posX, int posY): this(x: posX, y: posY);
 }
 
 class RobotCleaner {
-  
-  List<List<String>>? cleaningMap;
-  final StartingPosition startingPos;
+  List<List<String>> cleaningMap;
+  Position startingPos;
+  Position currentPos;
+  int steps = 0;
+  bool mapClean = false;
+  bool shouldPrintMap = true;
 
-  RobotCleaner({this.cleaningMap, this.startingPos = const StartingPosition()});
+  // Create a new map, starting position and current position of Robot
+  RobotCleaner({required this.cleaningMap, int startingX = 0, int startingY = 0}) :
+    startingPos = Position(x: startingX, y: startingY),
+    currentPos = Position(x: startingX, y: startingY);
 
   void printMap() {
-    cleaningMap?.forEach((row) { 
+    // Clear Debug Console
+    print("\x1B[2J\x1B[0;0H"); 
+    cleaningMap.forEach((row) { 
       String displayRow = row.fold('', (prev, curr) => '$prev $curr');
       print(displayRow);
     });
+    // Add delay to the printing of the map so that you can see how the Robot moves
+    sleep(Duration(milliseconds: 100));
   }
 
-  void start() {
-    cleaningMap?[startingPos.y][startingPos.x] = 'R';
-    cleaningMap != null ? traverseMap() : null;
+  int start(bool printMap) {
+    shouldPrintMap = printMap;
+    cleaningMap[startingPos.y][startingPos.x] = 'R';
+    traverseMap();
+    return steps;
   }
-
 
   void traverseMap() {
-
+    while(!isMapClean()!) {
+      if (canMoveUp()) {
+        cleaningMap[currentPos.y - 1][currentPos.x] = 'R';
+        cleaningMap[currentPos.y][currentPos.x] = '#';
+        currentPos = Position.from(currentPos.x, currentPos.y - 1);
+        steps++;
+      } else if (canMoveRight()) {
+        cleaningMap[currentPos.y][currentPos.x + 1] = 'R';
+        cleaningMap[currentPos.y][currentPos.x] = '#';
+        currentPos = Position.from(currentPos.x + 1, currentPos.y);
+        steps++;
+      } else if (canMoveDown()) {
+        cleaningMap[currentPos.y + 1][currentPos.x] = 'R';
+        cleaningMap[currentPos.y][currentPos.x] = '#';
+        currentPos = Position.from(currentPos.x, currentPos.y + 1);
+        steps++;
+      } else if (canMoveLeft()) {
+        cleaningMap[currentPos.y][currentPos.x - 1] = 'R';
+        cleaningMap[currentPos.y][currentPos.x] = '#';
+        currentPos = Position.from(currentPos.x - 1, currentPos.y);
+        steps++;
+      } else {
+        print('CANT MOVE');
+        mapClean = true;
+      }
+      shouldPrintMap ? printMap() : null;
+    }
   }
 
-  
+  // TODO: Clean up to only use a single method with parameters to calculate movability
+  bool canMoveUp() {
+    if (currentPos.y - 1 >= 0 && currentPos.y - 1 < cleaningMap.length) {
+      if (cleaningMap[currentPos.y - 1][currentPos.x] == '.') {
+        return true;
+      }
+    }
+    return false;
+  }
+  bool canMoveRight() {
+    if (currentPos.x + 1 >= 0 && currentPos.x + 1 < cleaningMap[0].length) {
+      if (cleaningMap[currentPos.y][currentPos.x + 1] == '.') {
+        return true;
+      }
+    }
+    return false;
+  }
+  bool canMoveDown() {
+    if (currentPos.y + 1 >= 0 && currentPos.y + 1 < cleaningMap.length) {
+      if (cleaningMap[currentPos.y + 1][currentPos.x] == '.') {
+        return true;
+      }
+    }
+    return false;
+  }
+  bool canMoveLeft() {
+    if (currentPos.x - 1 >= 0 && currentPos.x - 1 < cleaningMap[0].length) {
+      if (cleaningMap[currentPos.y][currentPos.x - 1] == '.') {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  bool? isMapClean() {
+    return cleaningMap.every((row) => row.every((cell) => cell != '.'));
+  }
 }
